c42e48142a97f0c23e1939c0f87db3940afb89df
anCheng <lzjtugjc@163.com>
2018-05-05 17:38:13 +0800

    完成收藏功能

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -15,7 +15,42 @@ Page({
   onLoad: function (options) {
     var postId = options.id;
     this.setData({
-      ...postsData.postList[postId]
+      ...postsData.postList[postId],
     });
+
+    var postsCollected = wx.getStorageSync('poset_Collected');
+    if (postsCollected) {
+      this.setData({
+        collected: postsCollected[postId]
+      });
+    } else {
+      var postsCollected = {};
+      postsCollected[postId] = false;
+      wx.setStorageSync('poset_Collected', postsCollected);
+    }
   },
+
+  onCollectionTap: function (event) {
+    var postsCollected = wx.getStorageSync('poset_Collected');
+    if (postsCollected) {
+      var postCollected = postsCollected[this.data.postId];
+      postsCollected[this.data.postId] = !postCollected;
+      wx.setStorageSync('poset_Collected', postsCollected);
+      // 更新数据绑定变量
+      this.setData({
+        collected: !postCollected
+      });
+
+      wx.showToast({
+        title: !postCollected ? '收藏成功' : '取消收藏成功',
+        duration: 1000,
+        icon: 'success'
+      })
+    }
+
+  },
+
+  onShareTap: function (event) {
+
+  }
 })
