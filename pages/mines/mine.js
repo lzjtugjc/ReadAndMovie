81a557c59d02c780461f51182e7ea05e8dcb2c02
anCheng <lzjtugjc@163.com>
2018-05-09 00:09:50 +0800

    初步完成我的页面

--- pages/mines/mine.js
+++ pages/mines/mine.js
@@ -20,13 +20,28 @@ Page({
     })
   },
 
-  onScanCodeTap:(event)=>{
+  onScanCodeTap: (event) => {
     // 允许从相机和相册扫码
     wx.scanCode({
       success: (res) => {
         wx.showToast({
           title: res.result,
         })
+      }
+    })
+  },
+
+  onAddressTap: (event) => {
+    wx.chooseAddress({
+      success: function (res) {
+        console.log(res.userName)
+        console.log(res.postalCode)
+        console.log(res.provinceName)
+        console.log(res.cityName)
+        console.log(res.countyName)
+        console.log(res.detailInfo)
+        console.log(res.nationalCode)
+        console.log(res.telNumber)
       }
     })
   },
